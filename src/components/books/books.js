3542c3cddbf2a27e6171adcc4841a50ca2dfc2b6
ntazelo <ntazelo@gmail.com>
2021-12-10 16:21:43 +0200

    Store book into store

--- src/components/books/books.js
+++ src/components/books/books.js
@@ -1,10 +1,25 @@
+import { useDispatch } from 'react-redux';
 import Book from './book/book';
 import './books.css';
+import { addBook } from '../../redux/books/books';
 
-const Books = () => (
-  <ul className="list-book-cont">
-    <Book />
-  </ul>
-);
+const Books = () => {
+  const dispatch = useDispatch();
+  const submitBookToStore = () => {
+    const newBook = {
+      id: Math.floor(Math.random() * 100000),
+      title: 'book',
+      author: 'author',
+    };
+    dispatch(addBook(newBook));
+  };
+
+  return (
+    <ul className="list-book-cont">
+      <Book />
+      <button type="button" onClick={submitBookToStore}>Add book</button>
+    </ul>
+  );
+};
 
 export default Books;
