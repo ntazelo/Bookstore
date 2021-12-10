54f896028233e09dd6ff5194ef6301ad403fb218
ntazelo <ntazelo@gmail.com>
2021-12-10 18:24:11 +0200

    Add and remove book into the store

--- src/components/books/book/book.js
+++ src/components/books/book/book.js
@@ -1,5 +1,28 @@
-const Book = () => (
-  <li className="book">Fear No Evil by James Patterson.</li>
-);
+/* eslint-disable react/prop-types */
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../../../redux/books/books';
+
+const Book = (props) => {
+  const { name, author, id } = props;
+  const dispatch = useDispatch();
+
+  const removeBookHandler = () => {
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <li className="book">
+      <span>
+        {name}
+        {' '}
+        by
+        {' '}
+        {author}
+      </span>
+
+      <button type="button" onClick={removeBookHandler} className="remove-book">Remove</button>
+    </li>
+  );
+};
 
 export default Book;
